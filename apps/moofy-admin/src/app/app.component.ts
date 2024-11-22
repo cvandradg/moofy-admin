95044e1f35689456799cb1aac6c3ebda3f31be5b
claudio <54946311+cvandradg@users.noreply.github.com>
2024-11-22 15:13:36 -0600

    feat: simplified styles

--- apps/moofy-admin/src/app/app.component.ts
+++ apps/moofy-admin/src/app/app.component.ts
@@ -1,24 +1,11 @@
-import { Component, inject } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
-import { AsyncPipe } from '@angular/common';
-import { ThemeManager } from '@moofy/shared';
-
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, MatMenuModule, AsyncPipe],
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'moofy-admin';
-  themeManager = inject(ThemeManager);
-  isDark$ = this.themeManager.isDark$;
-
-  changeTheme(theme: string) {
-    this.themeManager.changeTheme(theme);
-  }
-}
+export class AppComponent {}
--- libs/moofy-admin/frontend/login/src/lib/moofy-frontend-login/moofy-frontend-login.component.ts
+++ libs/moofy-admin/frontend/login/src/lib/moofy-frontend-login/moofy-frontend-login.component.ts
@@ -1,12 +1,31 @@
-import { MODULES } from '@moofy/shared';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { ThemeManager } from '@moofy/shared';
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { ChangeDetectionStrategy, inject, Component } from '@angular/core';
 
 @Component({
   selector: 'lib-moofy-frontend-login',
   standalone: true,
-  imports: [MODULES],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    AsyncPipe,
+    RouterModule,
+  ],
   templateUrl: './moofy-frontend-login.component.html',
   styleUrl: './moofy-frontend-login.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MoofyFrontendLoginComponent {}
+export class MoofyFrontendLoginComponent {
+  title = 'moofy-admin';
+  themeManager = inject(ThemeManager);
+  isDark$ = this.themeManager.isDark$;
+
+  changeTheme(theme: string) {
+    this.themeManager.changeTheme(theme);
+  }
+}
